6af2f60d4edf3072930fc11e1a975e16818198a4
myconcretelab <seb@myconcretelab.com>
2015-09-01 15:59:27 +0200

    Nettoyage du template Supermint pour le block Search
    Suppression de l’affichage des résultats & du bouton

--- blocks/search/templates/supermint.php
+++ blocks/search/templates/supermint.php
@@ -16,41 +16,4 @@
 	} ?>
 
 	<input name="query" type="text" value="<?php echo htmlentities($query, ENT_COMPAT, APP_CHARSET)?>" class="ccm-search-block-text" style="width:100%" />
-
-    <?php if($buttonText) { ?>
-	<input name="submit" type="submit" value="<?php echo h($buttonText)?>" class="btn btn-default ccm-search-block-submit" />
-    <?php } ?>
-
-<?php
-$tt = Loader::helper('text');
-if ($do_search) {
-	if(count($results)==0){ ?>
-		<h4 style="margin-top:32px"><?php echo t('There were no results found. Please try another keyword or phrase.')?></h4>
-	<?php }else{ ?>
-		<div id="searchResults">
-		<?php foreach($results as $r) {
-			$currentPageBody = $this->controller->highlightedExtendedMarkup($r->getPageIndexContent(), $query);?>
-			<div class="searchResult">
-				<h5><a href="<?php echo $r->getCollectionLink()?>"><?php echo $r->getCollectionName()?></a></h5>
-				<p>
-					<?php if ($r->getCollectionDescription()) { ?>
-						<?php  echo $this->controller->highlightedMarkup($tt->shortText($r->getCollectionDescription()),$query)?><br/>
-					<?php } ?>
-					<?php echo $currentPageBody; ?>
-					<a href="<?php  echo $r->getCollectionLink(); ?>" class="pageLink"><?php  echo $this->controller->highlightedMarkup($r->getCollectionLink(),$query)?></a>
-				</p>
-			</div>
-		<?php 	}//foreach search result ?>
-		</div>
-		<?php
-        $pages = $pagination->getCurrentPageResults();
-
-        if ($pagination->getTotalPages() > 1 && $pagination->haveToPaginate()) {
-            $showPagination = true;
-            echo $pagination->renderDefaultView();
-        }
-	} //results found
-}
-?>
-
 </form>
