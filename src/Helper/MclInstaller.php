856bfea7ed06249f1efe7a4e1197680187d8f4f6
myconcretelab <seb@myconcretelab.com>
2015-09-25 13:38:12 +0200

    Issues updates
    - Fix the First level Border bottom default color of a selected top
    nav” where nothing happens when we choose a color
    - Now « Site Logo » display blocks from left to right in mobile top-bar
    - Add different Stack to installation.
    - New documentation & demo pages

--- src/Helper/MclInstaller.php
+++ src/Helper/MclInstaller.php
@@ -60,7 +60,6 @@ class MclInstaller
         $this->importPageTemplates($sx);
         $this->importFileImportantThumbnailTypes($sx);
         $this->importSystemContentEditorSnippets($sx);
-        $this->importStacksContent($sx);
     }
 
     protected function getPackageObject()
@@ -95,6 +94,7 @@ class MclInstaller
             }
         }
     }
+
 
     protected function importPageTemplates(\SimpleXMLElement $sx)
     {
@@ -301,6 +301,54 @@ class MclInstaller
                     }
 
                 }
+            }
+        }
+    }
+    public function importPageAreas(Page $page, \SimpleXMLElement $px)
+    {
+        foreach ($px->area as $ax) {
+            if (isset($ax->blocks)) {
+                foreach ($ax->blocks->block as $bx) {
+                    if ($bx['type'] != '') {
+                        // we check this because you might just get a block node with only an mc-block-id, if it's an alias
+                        $bt = BlockType::getByHandle((string) $bx['type']);
+                        if (!is_object($bt)) {
+                            throw new \Exception(t('Invalid block type handle: %s', strval($bx['type'])));
+                        }
+                        $btc = $bt->getController();
+                        $btc->import($page, (string) $ax['name'], $bx);
+                    } else {
+                        if ($bx['mc-block-id'] != '') {
+                            // we find that block in the master collection block pool and alias it out
+                            $bID = array_search((string) $bx['mc-block-id'], self::$mcBlockIDs);
+                            if ($bID) {
+                                $mc = Page::getByID($page->getMasterCollectionID(), 'RECENT');
+                                $block = Block::getByID($bID, $mc, (string) $ax['name']);
+                                $block->alias($page);
+
+                                if ($block->getBlockTypeHandle() == BLOCK_HANDLE_LAYOUT_PROXY) {
+                                    // we have to go get the blocks on that page in this layout.
+                                    $btc = $block->getController();
+                                    $arLayout = $btc->getAreaLayoutObject();
+                                    $columns = $arLayout->getAreaLayoutColumns();
+                                    foreach ($columns as $column) {
+                                        $area = $column->getAreaObject();
+                                        $blocks = $area->getAreaBlocksArray($mc);
+                                        foreach ($blocks as $_b) {
+                                            $_b->alias($page);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (isset($ax->style)) {
+                $area = \Area::get($page, (string) $ax['name']);
+                $set = StyleSet::import($ax->style);
+                $page->setCustomStyleSet($area, $set);
             }
         }
     }
