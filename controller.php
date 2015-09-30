c84c475e984d8ba1d2c97bfd510de92ae5757488
myconcretelab <seb@myconcretelab.com>
2015-09-30 06:50:48 +0200

    Version change

--- controller.php
+++ controller.php
@@ -35,7 +35,7 @@ class Controller extends Package  {
 	protected $pkgHandle = 'theme_supermint';
     protected $themeHandle = 'supermint';
 		protected $appVersionRequired = '5.7.4';
-		protected $pkgVersion = '3.2.2';
+		protected $pkgVersion = '3.2.2.1';
 		protected $pkg;
     protected $pkgAllowsFullContentSwap = true;
     protected $startingPoint;
