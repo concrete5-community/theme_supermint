b57992eecea621394597ce137cd8e0c87cd44fa1
Jacqmin Sébastien <seb@tellthem.be>
2016-02-18 08:26:33 +0100

    Fix multiple popup issue
    http://www.concrete5.org/marketplace/themes/supermint-3/support/multiple
    -pop-ups-on-a-page-all-show-the-same-stack/

--- blocks/core_stack_display/templates/supermint_popup.php
+++ blocks/core_stack_display/templates/supermint_popup.php
@@ -6,11 +6,11 @@ $o = \Concrete\Package\ThemeSupermint\Src\Models\ThemeSupermintOptions::get();
 $c = Page::getCurrentPage();
 $cp = new Permissions($c);
 if ($cp->canViewPageVersions()) {
-	$stack = Stack::getByID($stID);	
+	$stack = Stack::getByID($stID);
 } else {
 	$stack = Stack::getByID($stID, 'ACTIVE');
 }
-if (is_object($stack)) { 
+if (is_object($stack)) {
 	$ax = Area::get($stack, STACKS_AREA_NAME);
 	$axp = new Permissions($ax);
 	if ($axp->canRead()) {
@@ -22,8 +22,8 @@ if (is_array($blocks) && count($blocks)): ?>
 <div class="sm-stack-popup">
 	<?php foreach ($blocks as $key => $block) :
 	$bv = new BlockView($block); ?>
-	<a href="#stack-popup-<?php echo $key?>" class="<?php echo $o->popup_button_type . ' ' . $o->popup_button_color ?> mpf-inline open-popup-link"><?php echo $block->getBlockName() ? $block->getBlockName() : t('Title ') . $key ?></a>
-	<div class='white-popup mfp-hide' id="stack-popup-<?php echo $key?>"><?php echo $bv->render('view') ?></div>
+	<a href="#stack-popup-<?php echo $bID . '-' . $key?>" class="<?php echo $o->popup_button_type . ' ' . $o->popup_button_color ?> mpf-inline open-popup-link"><?php echo $block->getBlockName() ? $block->getBlockName() : t('Title ') . $key ?></a>
+	<div class='white-popup mfp-hide' id="stack-popup-<?php echo $bID . '-' . $key?>"><?php echo $bv->render('view') ?></div>
 	<?php endforeach ?>
 </div>
 <?php endif ?>
