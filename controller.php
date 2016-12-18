83e5a9966cb6b0874dddd90020d20e2a87a835c9
Jacqmin Sébastien <seb@tellthem.be>
2016-12-18 16:05:25 +0100

    Fix controller first sep

--- controller.php
+++ controller.php
@@ -28,6 +28,7 @@ use PageList;
 use StackList;
 use Config;
 use Concrete\Core\StyleCustomizer\Style\ValueList;
+use Concrete\Core\Package\ContentSwapper;
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
@@ -39,7 +40,7 @@ class Controller extends Package  {
 		protected $pkgVersion = '3.3.4.1';
 		protected $pkg;
     protected $pkgAllowsFullContentSwap = true;
-    protected $startingPoint;
+    public $startingPoint;
 
 	public function getPackageDescription() {
 		return t("Supermint responsive suit any kind of website.");
@@ -254,86 +255,98 @@ class Controller extends Package  {
         );
     }
 
-    public function swapContent($options) {
 
-        if ($this->validateClearSiteContents($options)) {
-            \Core::make('cache/request')->disable();
+		public function getContentSwapper() {
+			return new MclContentSwapper();
+		}
 
-            $pl = new PageList();
-            $pages = $pl->getResults();
-            foreach ($pages as $c) $c->delete();
 
-            $fl = new FileList();
-            $files = $fl->getResults();
-            foreach ($files as $f) $f->delete();
-
-            // clear stacks
-            $sl = new StackList();
-            foreach ($sl->get() as $c) $c->delete();
-
-            $home = Page::getByID(HOME_CID);
-            $blocks = $home->getBlocks();
-            foreach ($blocks as $b) $b->deleteBlock();
-
-            $pageTypes = PageType::getList();
-            foreach ($pageTypes as $ct) $ct->delete();
-
-						$startingPointFolder = $this->getPackagePath() . '/starting_points/'. $this->startingPoint;
-
-            // Import Files
-            if (is_dir($startingPointFolder . '/content_files')) {
-                $ch = new ContentImporter();
-                $computeThumbnails = true;
-                if ($this->contentProvidesFileThumbnails()) $computeThumbnails = false;
-                $ch->importFiles($startingPointFolder . '/content_files', true );
-            }
-
-            // Install the starting point.
-            if (is_file($startingPointFolder . '/content.xml')) :
-                $ci = new ContentImporter();
-                $ci->importContentFile($startingPointFolder . '/content.xml');
-            endif;
-
-            // Set it as default for the page theme
-            $this->setPresetAsDefault($this->startingPoint);
-
-            // Restore Cache
-            \Core::make('cache/request')->enable();
-        }
-    }
-
-    function setPresetAsDefault ($presetHandle) {
-        $outputError = false;
-        $baseExceptionText = t('The theme and the Starting point has been installed correctly but it\'s ');
-        $pt = PageTheme::getByHandle($this->themeHandle);
-        $preset = $pt->getThemeCustomizablePreset($presetHandle);
-        if (!is_object($preset)) {
-            if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Preset selected : ' . $presetHandle));
-            return;
-        }
-        $styleList = $pt->getThemeCustomizableStyleList();
-        if (!is_object($styleList)) {
-            if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Style List from ' . $presetHandle));
-            return;
-        }
-        $valueList = $preset->getStyleValueList();
-        $vl = new ValueList();
-
-        $sets = $styleList->getSets();
-        if (!is_array($sets)) {
-            if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Style Set from ' . $presetHandle));
-            return;
-        }
-
-        foreach ($sets as $set) :
-         foreach($set->getStyles() as $style)  :
-            $valueObject = $style->getValueFromList($valueList);
-            if (is_object($valueObject))
-                $vl->addValue($valueObject);
-         endforeach;
-        endforeach;
-
-        $vl->save();
-        $pt->setCustomStyleObject($vl, $preset);
-    }
 }
+
+class MclContentSwapper extends ContentSwapper {
+
+	public function swapContent (Package $package, $options) {
+
+			if ($this->validateClearSiteContents($options)) {
+					\Core::make('cache/request')->disable();
+
+					$pl = new PageList();
+					$pages = $pl->getResults();
+					foreach ($pages as $c) $c->delete();
+
+					$fl = new FileList();
+					$files = $fl->getResults();
+					foreach ($files as $f) $f->delete();
+
+					// clear stacks
+					$sl = new StackList();
+					foreach ($sl->get() as $c) $c->delete();
+
+					$home = Page::getByID(HOME_CID);
+					$blocks = $home->getBlocks();
+					foreach ($blocks as $b) $b->deleteBlock();
+
+					$pageTypes = PageType::getList();
+					foreach ($pageTypes as $ct) $ct->delete();
+
+					$startingPointFolder = $package->getPackagePath() . '/starting_points/'. $package->startingPoint;
+
+					// Import Files
+					if (is_dir($startingPointFolder . '/content_files')) {
+							$ch = new ContentImporter();
+							$computeThumbnails = true;
+							if ($package->contentProvidesFileThumbnails()) $computeThumbnails = false;
+							$ch->importFiles($startingPointFolder . '/content_files', true );
+					}
+
+					// Install the starting point.
+					if (is_file($startingPointFolder . '/content.xml')) :
+						// var_dump($startingPointFolder); die(' TS ');
+							$ci = new ContentImporter();
+							$ci->importContentFile($startingPointFolder . '/content.xml');
+					endif;
+
+					// Set it as default for the page theme
+					$this->setPresetAsDefault($package->startingPoint);
+
+					// Restore Cache
+					\Core::make('cache/request')->enable();
+			}
+	}
+
+	function setPresetAsDefault ($presetHandle) {
+			$outputError = false;
+			$baseExceptionText = t('The theme and the Starting point has been installed correctly but it\'s ');
+			$pt = PageTheme::getByHandle($package->themeHandle);
+			$preset = $pt->getThemeCustomizablePreset($presetHandle);
+			if (!is_object($preset)) {
+					if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Preset selected : ' . $presetHandle));
+					return;
+			}
+			$styleList = $pt->getThemeCustomizableStyleList();
+			if (!is_object($styleList)) {
+					if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Style List from ' . $presetHandle));
+					return;
+			}
+			$valueList = $preset->getStyleValueList();
+			$vl = new ValueList();
+
+			$sets = $styleList->getSets();
+			if (!is_array($sets)) {
+					if($outputError) throw new \Exception($baseExceptionText . t('impossible to retrieve the Style Set from ' . $presetHandle));
+					return;
+			}
+
+			foreach ($sets as $set) :
+			 foreach($set->getStyles() as $style)  :
+					$valueObject = $style->getValueFromList($valueList);
+					if (is_object($valueObject))
+							$vl->addValue($valueObject);
+			 endforeach;
+			endforeach;
+
+			$vl->save();
+			$pt->setCustomStyleObject($vl, $preset);
+	}
+
+	}
